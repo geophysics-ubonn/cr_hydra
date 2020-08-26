1e0265873a478433ab1623a0a3ea303db375c73f
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2020-08-26 11:38:59 +0200

    sql scheme

--- database_scheme/tables.sql
+++ database_scheme/tables.sql
@@ -1,18 +1,32 @@
--- inversions
+-- we store tomodirs (unfinished and finished) in the database
+drop table if exists "binary_data" cascade;
 
+create table "binary_data" (
+	index SERIAL PRIMARY KEY,
+	filename text,
+	hash text,
+	bytea data
+);
+
+-- inversions
 drop table if exists "inversions" cascade;
 
 create table "inversions" (
 	index SERIAL PRIMARY KEY,
+	--
 	username text,
 	datetime_init timestamptz,
-    archive_file text,
-	archive_hash text,
-    computer text,
+    tomodir_unfinished_file integer references binary_data (index),
+    tomodir_finished_file integer references binary_data (index),
+    source_computer text,
+	-- mod or inv
 	sim_type text,
-    hydra_location text,
+    -- hydra_location text,
+	-- path to crh meta data file (replaces the actual tomodir) on
+	-- source_computer
     crh_file text,
+	-- everything is uploaded
 	ready_for_processing BOOLEAN DEFAULT False,
+	-- set to "finished" when inversion is ready
 	status text default 'unfinished'
 );
-
